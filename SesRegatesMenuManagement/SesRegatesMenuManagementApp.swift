a9671140e10026bc8000f3fcd9a10203ec8861eb
Willy <guillecg13@gmail.com>
2024-06-28 18:53:21 +0200

    Initial Commit

---
+++ SesRegatesMenuManagement/SesRegatesMenuManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  SesRegatesMenuManagementApp.swift
+//  SesRegatesMenuManagement
+//
+//  Created by Willy on 28/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SesRegatesMenuManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
