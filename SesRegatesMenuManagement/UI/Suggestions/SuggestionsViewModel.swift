07f11b5cc5f97fab2134c2eb0fefcdaaa54ffc57
Willy <guillecg13@gmail.com>
2024-10-15 17:47:51 +0200

    Added: createDish and editDish functionalities

--- SesRegatesMenuManagement/UI/Suggestions/SuggestionsViewModel.swift
+++ SesRegatesMenuManagement/UI/Suggestions/SuggestionsViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+final class SuggestionsViewModel: ObservableObject {
+    @Published var suggestionsMenu: [Dish]
+    
+    init(suggestions: [Dish]) {
+        self.suggestionsMenu = suggestions
+    }
+    
+    func createDish(plate: String, price: String){
+        let newDish = Dish(name: plate, price: Float(price) ?? 0)
+        self.suggestionsMenu.append(newDish)
+    }
+    
+    func editDish(dish: Dish, plate: String, price: String) {
+        if let index = self.suggestionsMenu.firstIndex(of: dish) {
+            self.suggestionsMenu[index] = Dish(name: plate, price: (price as NSString).floatValue)
+        }
+    }
+}
