374192bfb4cc0c82e68f1ad96f4f012dd3aecd0e
Willy <guillecg13@gmail.com>
2024-10-15 17:40:39 +0200

    Added: suggestion mockup

--- SesRegatesMenuManagement/Domain/Mockups/SuggestionMockupList.swift
+++ SesRegatesMenuManagement/Domain/Mockups/SuggestionMockupList.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+let suggestionMockupList: SuggestionsMenu = SuggestionsMenu(suggestions: [
+            Dish(name: "Salmón con mango y pistachos", price: 19.00),
+            Dish(name: "Atún en costra con mayonesa de soja y brotes", price: 16.50),
+            Dish(name: "Lenguado menieur", price: 24.00),
+            Dish(name: "Chuletas de cordero fritas con ajos", price: 17.90),
+            Dish(name: "Cachopo con patatas y pimientos de padrón", price: 19.00)
+        ]
+    )
