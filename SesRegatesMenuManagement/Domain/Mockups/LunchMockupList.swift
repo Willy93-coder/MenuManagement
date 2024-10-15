2cfe3da420536c1f8225ef31190d33332c251c79
Willy <guillecg13@gmail.com>
2024-10-15 17:40:21 +0200

    Added: lunch mockup

--- SesRegatesMenuManagement/Domain/Mockups/LunchMockupList.swift
+++ SesRegatesMenuManagement/Domain/Mockups/LunchMockupList.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+let lunchMockupList: MainMenu = MainMenu(
+    menu: ["Entrants": [
+                Dish(name: "Croquetes variades", price: 7.50),
+                Dish(name: "Croquetes de bullit", price: 6.50),
+                Dish(name: "Aletes de pollastre", price: 7.50),
+                Dish(name: "Frit mallorquí", price: 10.95),
+                Dish(name: "Frit de matances", price: 7.50),
+                Dish(name: "Caragols", price: 7.50),
+            ],
+           "Aminades": [
+                Dish(name: "Amanida Cèsar", price: 10.95),
+                Dish(name: "Amanida de Formatge de Cabra", price: 11.95),
+                Dish(name: "Amanida mixta amb tonyina", price: 10.95),
+           ],
+           "Primers": [
+                Dish(name: "Llom amb col", price: 12.95),
+                Dish(name: "Albergínies farcides", price: 11.50),
+                Dish(name: "Conill amb ceba", price: 10.95),
+                Dish(name: "Mig pollastre a l’ast amb patates", price: 9.50),
+                Dish(name: "Costella de Porc rostida", price: 10.95),
+           ],
+           "Carns": [
+                Dish(name: "Guàtleres a la planxa (2u)", price: 11.90),
+                Dish(name: "Entrecot", price: 16.50),
+                Dish(name: "Chuletón 500g.", price: 23.50),
+                Dish(name: "Solomillo", price: 19.50),
+                Dish(name: "Xot rostit", price: 27.90),
+                Dish(name: "Porcella", price: 16.95),
+           ],
+           "Peix": [
+            Dish(name: "Calamar Nacional", price: 20.00),
+            Dish(name: "Salmó a la planxa", price: 15.00),
+            Dish(name: "Llobarro a la planxa (700gr)", price: 22.00),
+           ],
+           "Arroç": [
+                Dish(name: "Paella Mixta (2p. min)", price: 14.95),
+                Dish(name: "Paella de Marisc (2p. min)", price: 15.95),
+                Dish(name: "Paella Negra (2p. min)", price: 14.95),
+                Dish(name: "Paella de Bogavante (2p. min)", price: 24),
+                Dish(name: "Arroç Brut", price: 14.95),
+           ],
+           "Postres Casolans": [
+                Dish(name: "Gató amb gelat", price: 5.50),
+                Dish(name: "Pastís de formatge", price: 5.50),
+                Dish(name: "Pastís de pastanaga", price: 5.50),
+                Dish(name: "Pastís red velvet", price: 5.50)
+           ]
+    ]
+)
+
