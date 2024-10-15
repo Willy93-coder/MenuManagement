3859f7fd7ecd726d915248a22a6891df46b6e738
Willy <guillecg13@gmail.com>
2024-10-15 17:45:28 +0200

    Added: edit view components

--- SesRegatesMenuManagement/UI/Edit/EditUI.swift
+++ SesRegatesMenuManagement/UI/Edit/EditUI.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct EditUI: View {
+    
+    @ObservedObject var viewModel: SuggestionsViewModel
+    var dish: Dish?
+    @State private var plate: String
+    @State private var price: String
+    @FocusState private var isFocus: FormFieldFocus?
+    
+    enum FormFieldFocus: Hashable {
+        case dish, price
+    }
+    
+    init(viewModel: SuggestionsViewModel, dish: Dish? = nil) {
+        self.viewModel = viewModel
+        self.dish = dish
+        _plate = State(initialValue: dish?.name ?? "")
+        _price = State(initialValue: dish != nil ? String(dish!.price) : "")
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Plato", text: $plate).focused($isFocus, equals: .dish)
+                    TextField("Precio", text: $price).focused($isFocus, equals: .price).keyboardType(.decimalPad)
+                }
+                Section{
+                    Button("Guardar") {
+                        if self.dish != nil {
+                            viewModel.editDish(dish: dish!, plate: self.plate, price: self.price)
+                        } else {
+                            viewModel.createDish(plate: self.plate, price: self.price)
+                        }
+                    }
+                }.disabled(plate.isEmpty || price.isEmpty)
+                
+                // Add toast component to inform the dish is saved.
+            }
+            .navigationTitle("Edición del plato" )
+        }
     }
 }
 
-#Preview {
-    EditUI()
-}
+
